6b6e582751e474d7db5e6481b60975102b6eb131
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-11-20 09:48:06 -0800

    made the newKegForm.js file and used the reusable form data init.now making the editkegform.js file

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+
+function NewKegForm (props){
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewItemCreation({
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      alcoholContent: event.target.alcoholContent.value,
+      price: event.target.price.value,
+      id: v4()});
+  }
+  return (
+    <React.Fragment>
+        <div className="container"> 
+          <ReusableForm
+            formSubmissionHandler = {handleNewKegFormSubmission}
+            buttonText= "Add Keg" />      
+        </div>
+    </React.Fragment>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewItemCreate: PropTypes.func
+}
+
+export default NewKegForm;
