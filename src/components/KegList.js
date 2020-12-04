a3bef0a18abacad951b0b2f6550bc1bf32dc21a2
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-12-04 12:45:13 -0800

    going back over each line of code at this time to find the bug

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,30 +1,27 @@
 import React from "react";
-import Keg from './Keg';
-import PropTypes from 'prop-types'
+import Keg from "./Keg";
+import PropTypes from "prop-types";
+
 function KegList(props){
-  return (
+  return(
     <React.Fragment>
-      <hr />
-      {/* We now need to map over the values of an object, not an array. */}
-      {Object.values(props.kegList).map((keg) =>{
-        return <Keg
-          whenKegClicked = { props.onKEgSelection }
-          name={keg.names}
-          brand={keg.brand}
-          price={keg.price}
+      <hr/>
+      {Object.values(props.kegList).map((keg) =>
+        <Keg
+          whenKegClicked = {props.onKegSelection}
+          name = {keg.name}
+          brand = {keg.brand}
+          price = {keg.price}
           alcoholContent = {keg.alcoholContent}
           pintsLeft = {keg.pintsLeft}
           id={keg.id}
           key={keg.id}/>
-       })}
-      {/* Don't forget to add the curly brace above - otherwise there will be a syntax error. */}
+      )}
     </React.Fragment>
   );
 }
 
-
 KegList.propTypes = {
-  // The PropType below has been updated - it's now an object, not an array.
   kegList: PropTypes.object,
   onKegSelection: PropTypes.func
 };
