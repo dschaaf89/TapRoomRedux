b6fc8ccb3953490b3b8ad352416f46d017a3178e
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-12-04 08:39:47 -0800

    added second test for adding a new Keg

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,37 @@
 import kegListReducer from '../../reducers/keg-list-reducers';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+  name: 'Pale Ale',
+  brand: 'Sam Adams',
+  price: '4.99',
+  alcoholContent:'6.5',
+  id:1
+  };
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brand, price,alcoholContent, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        id: id
+      }
+    });
+  });
+
 });
