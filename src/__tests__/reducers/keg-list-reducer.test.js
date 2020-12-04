9fdefd4bca6e117e7006f796f75be90e5ff2b27b
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-12-04 08:45:21 -0800

    wrote the delete keg test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -9,6 +9,24 @@ describe('kegListReducer', () => {
   alcoholContent:'6.5',
   id:1
   };
+
+  const currentState = {
+    1:{
+    name: 'Pale Ale',
+    brand: 'Sam Adams',
+    price: '4.99',
+    alcoholContent:'6.5',
+    id:1
+    },
+    2:{
+    name: 'Octoberfest',
+    brand: 'Sam Adams',
+    price: '6.99',
+    alcoholContent:'6.5',
+    id:2
+
+    }
+  }
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
@@ -33,5 +51,21 @@ describe('kegListReducer', () => {
       }
     });
   });
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+    expect(kegListReducer(currentState, action)).toEqual({
+      2: {
+        name: 'Octoberfest',
+        brand: 'Sam Adams',
+        price: '6.99',
+        alcoholContent:'6.5',
+        id:2
+      }
+    });
+  });
+
 
 });
