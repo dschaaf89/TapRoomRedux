000fbc9cdc4f229f9729052d7a138373aef4ce62
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-12-04 08:10:27 -0800

    cloned react Taproom and set up project. setting up to use Redux for state now wrote the first test so i can set up the reducers

---
+++ src/components/__tests__/reducers/keg-list-reducer.test.js
@@ -0,0 +1,7 @@
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+escribe('kegListReducer', () => {
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+});
